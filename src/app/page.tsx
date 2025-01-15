0d9829769d931602a8595e1a1d35ba0309004261
Alizah Fatima <161517889+alizah-fatima@users.noreply.github.com>
2025-01-15 11:27:10 +0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Hero from "@/components/hero";
 import Products from "@/components/products";
 import Offer from "@/components/offer";
